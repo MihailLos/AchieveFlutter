0727a00322f040f26a101360323ee5a4b3e3066f
ender <enderclub12@gmail.com>
2022-02-16 22:08:12 +0700

    Создание ViewModel для экрана обучающегося

---
+++ lib/screens/profile/profile_viewmodel.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:stacked/stacked.dart';
+
+class ProfileViewModel extends BaseViewModel {
+  ProfileViewModel(BuildContext context);
+
+  onReady() {
+
+  }
+
+}
