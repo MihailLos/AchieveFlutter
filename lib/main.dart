581aa718e11014e93f3e402ca5f07b6a9e656342
ender <enderclub12@gmail.com>
2022-02-16 22:07:33 +0700

    Предпросмотр экрана профиля

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:achieve_student_flutter/screens/profile/profile_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:achieve_student_flutter/screens/login_screen.dart';
+import 'package:achieve_student_flutter/screens/login/login_screen.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: LoginScreen(),
+    home: ProfileScreen(),
   ));
 }
