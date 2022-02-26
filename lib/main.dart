2dfcdef546673c53e56f4dd4026ec59b21787144
ender <enderclub12@gmail.com>
2022-02-26 19:56:04 +0700

    Начальный экран - экран авторизации

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:achieve_student_flutter/screens/home_view.dart';
 import 'package:achieve_student_flutter/screens/profile/profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:achieve_student_flutter/screens/login/login_screen.dart';
@@ -5,6 +6,6 @@ import 'package:achieve_student_flutter/screens/login/login_screen.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: ProfileScreen(),
+    home: LoginScreen(),
   ));
 }
