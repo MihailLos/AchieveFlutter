a79aaa70291f6b3c2a79f685eae85c6aeb809ff8
ender <enderclub12@gmail.com>
2022-02-26 19:57:35 +0700

    Изменение структуры профиля, получение данных с сервера

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:stacked/stacked.dart';
 import 'profile_viewmodel.dart';
 
@@ -14,96 +17,258 @@ class ProfileScreen extends StatelessWidget {
     return ViewModelBuilder<ProfileViewModel>.reactive(
         viewModelBuilder: () => ProfileViewModel(context),
         onModelReady: (viewModel) => viewModel.onReady(),
-        builder: (context, model, chile) {
+        builder: (context, model, child) {
           return Scaffold(
             appBar: _appBar(context, model),
             body: _body(context, model),
-            bottomNavigationBar: _bottomNavBar(context, model),
           );
         });
   }
 
-  _appBar(BuildContext context, ProfileViewModel model) {
+  _appBar(context, model) {
     return AppBar(
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.report,
-                color: Colors.blueAccent,
-                size: 32,
-              )),
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.login,
-                color: Color(0xFFFF9966),
-                size: 32,
-              ))
-        ],
+      leading: IconButton(
+        icon: Image.asset("assets/images/support_button.png"),
+        onPressed: () {},
+        iconSize: 32,
       ),
-      backgroundColor: Colors.transparent,
       elevation: 0,
-    );
-  }
-
-  _body(BuildContext context, ProfileViewModel model) {
-    return SafeArea(child: _profile(context, model));
-  }
-
-  _bottomNavBar(BuildContext context, ProfileViewModel model) {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      items: [
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.school,
-            size: 32,
-          ),
-          label: "Профиль",
+      backgroundColor: Colors.transparent,
+      actions: [
+        IconButton(
+          icon: Image.asset("assets/images/exit_button.png"),
+          onPressed: () => model.exitButtonAction(context),
+          iconSize: 32,
         ),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.bar_chart, size: 32), label: "Рейтинг"),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.star, size: 32), label: "Достижения"),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.format_list_bulleted, size: 32), label: "Заявки"),
       ],
-      selectedItemColor: Colors.blueAccent,
-      unselectedItemColor: Colors.black,
-      selectedLabelStyle: Theme.of(context).textTheme.caption,
-      unselectedLabelStyle: Theme.of(context).textTheme.caption,
     );
   }
 
-  _profile(BuildContext context, ProfileViewModel model) {
+  _body(context, model) {
+    return ListView(
+      physics: BouncingScrollPhysics(),
+      children: [
+        _profilePictureWidget(context, model),
+        SizedBox(
+          height: 16,
+        ),
+        _profileName(context, model),
+        SizedBox(
+          height: 16,
+        ),
+        _progressField(context, model),
+        SizedBox(
+          height: 8,
+        ),
+        _educationInfo(context, model)
+      ],
+    );
+  }
+
+  _profilePictureWidget(context, model) {
     return Center(
-      child: Column(
+      child: Stack(
         children: [
           _profilePicture(context, model),
-          _profileName(context, model)
+          Positioned(
+            child: _editProfilePictureButton(context, model),
+            bottom: 0,
+            right: 4,
+          )
         ],
       ),
     );
   }
 
-  _profilePicture(BuildContext context, ProfileViewModel model) {
-    return CircleAvatar(
-      backgroundImage: AssetImage("assets/support_image.png"),
-      radius: 47.5,
+  _profilePicture(context, model) {
+    final image = MemoryImage(model.getImageFromBase64());
+
+    return model.circular
+        ? LinearProgressIndicator()
+        : ClipOval(
+            child: Material(
+              color: Colors.transparent,
+              child: Ink.image(
+                image: image,
+                fit: BoxFit.cover,
+                width: 95,
+                height: 95,
+                child: InkWell(
+                  onTap: () {
+                    showModalBottomSheet(
+                        context: context,
+                        builder: ((builder) =>
+                            _bottomChooseProfilePhotoWidget(context, model)));
+                  },
+                ),
+              ),
+            ),
+          );
+  }
+
+  _profileName(context, model) {
+    return model.circular
+        ? LinearProgressIndicator()
+        : Center(
+            child: Text(
+              model.studentProfileModel.firstName +
+                  " " +
+                  model.studentProfileModel.lastName,
+              style: GoogleFonts.montserrat(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.normal),
+            ),
+          );
+  }
+
+  _editProfilePictureButton(context, model) {
+    return _buildCircle(
+      Icon(
+        Icons.edit,
+        size: 20,
+        color: Colors.white,
+      ),
     );
   }
 
-  _profileName(BuildContext context, ProfileViewModel model) {
-    return Text(
-      "Иван Иванов",
-      style: TextStyle(
-          fontFamily: "Montseratt",
-          fontSize: 14,
-          fontWeight: FontWeight.w600,
-          height: 17),
+  _buildCircle(Widget child) {
+    return ClipOval(
+      child: Container(
+        padding: EdgeInsets.all(5),
+        color: Colors.blueAccent,
+        child: child,
+      ),
+    );
+  }
+
+  _progressField(context, model) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _userScore(context, model),
+          _progressBar(context, model),
+          _userPercent(context, model)
+        ],
+      ),
+    );
+  }
+
+  _userScore(context, model) {
+    return model.circular
+        ? LinearProgressIndicator()
+        : Row(
+            children: [
+              Text(
+                model.studentProfileModel.score.toString(),
+                style: GoogleFonts.montserrat(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.normal,
+                    color: Colors.blueAccent),
+              ),
+              Image(image: AssetImage("assets/images/prize_icon.png"))
+            ],
+          );
+  }
+
+  _progressBar(context, model) {
+    return model.circular
+        ? LinearProgressIndicator()
+        : SizedBox(
+            height: 29,
+            width: 202,
+            child: LinearProgressIndicator(
+              value: model.studentPercentProgressBar,
+            ),
+          );
+  }
+
+  _userPercent(context, model) {
+    return model.circular
+        ? LinearProgressIndicator()
+        : Text(model.studentPercent.toString() + "%",
+            style: GoogleFonts.montserrat(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.normal,
+              color: Colors.orange,
+            ));
+  }
+
+  _educationInfo(context, model) {
+    return model.circular
+        ? LinearProgressIndicator()
+        : Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 29),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(model.studentProfileModel.instituteFullName),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(model.studentProfileModel.streamFullName),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(model.studentProfileModel.groupName),
+              ],
+            ),
+          );
+  }
+
+  _bottomChooseProfilePhotoWidget(context, model) {
+    return Container(
+      height: 150,
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+      child: Center(
+        child: Column(
+          children: [
+            Text(
+              "Изменить фото профиля",
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Text(
+              "Если фото будет содержать непристойный контент, Ваш аккаунт будет заблокирован модератором!",
+              style: TextStyle(fontSize: 14),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {model.pickPhoto(ImageSource.camera);},
+                      icon: Icon(Icons.camera_alt_outlined),
+                      iconSize: 40,
+                    ),
+                    Text("Камера", style: TextStyle(fontSize: 14),)
+                  ],
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {model.pickPhoto(ImageSource.gallery);},
+                      icon: Icon(Icons.image_outlined),
+                      iconSize: 40,
+                    ),
+                    Text("Галерея", style: TextStyle(fontSize: 14),)
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/screens/profile/profile_viewmodel.dart
+++ lib/screens/profile/profile_viewmodel.dart
@@ -1,11 +1,88 @@
+import 'dart:convert';
+import 'dart:ffi';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:achieve_student_flutter/model/student_profile.dart';
+import 'package:achieve_student_flutter/network_handler.dart';
+import 'package:convert/convert.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:stacked/stacked.dart';
+import 'package:path/path.dart';
 
 class ProfileViewModel extends BaseViewModel {
   ProfileViewModel(BuildContext context);
 
-  onReady() {
+  int currentBottomNavBarIndex = 0;
+  NetworkHandler networkHandler = NetworkHandler();
+  StudentProfileModel studentProfileModel = StudentProfileModel();
+  int? studentPercent;
+  bool circular = true;
+  File? profileImageFile;
+  FlutterSecureStorage tokenStorage = FlutterSecureStorage();
+  double? studentPercentProgressBar;
 
+  Future onReady() async {
+    fetchData();
+    fetchStudentPercent();
   }
 
+  void fetchData() async {
+    var response = await networkHandler.get("/student/getStudent");
+    studentProfileModel = StudentProfileModel.fromJson(response);
+    circular = false;
+    notifyListeners();
+  }
+
+  void fetchStudentPercent() async {
+    var response = await networkHandler.get("/student/achievementsPercent");
+    studentPercent = response;
+    studentPercentProgressBar = studentPercent! / 100;
+    circular = false;
+    notifyListeners();
+  }
+
+  exitButtonAction(context) {
+    Navigator.pop(context);
+  }
+
+  getImageFromBase64() {
+    var firstDecode = base64Decode(studentProfileModel.data.toString());
+    String secondDecode = latin1.decode(firstDecode);
+    var decodedImage = base64Decode(secondDecode);
+    circular = false;
+    return decodedImage;
+  }
+
+  getImageFromGallery() async {
+    String? accessToken = await tokenStorage.read(key: "token");
+    XFile? pickedFile = await ImagePicker()
+        .pickImage(source: ImageSource.gallery, maxWidth: 462, maxHeight: 462);
+    if (pickedFile != null) {
+      File imageFile = File(pickedFile.path);
+      var firstEncode = base64Encode(imageFile.readAsBytesSync());
+      Codec<String, String> stringToBase64Url = utf8.fuse(base64Url);
+      var secondEncode = stringToBase64Url.encode(firstEncode);
+      Map<String, dynamic> bodyData = {
+        "data": firstEncode,
+        "format": "png",
+        "listFileId": null
+      };
+      var response = await networkHandler.put("/student/changePhoto", {"Authorization": "Bearer $accessToken"}, bodyData);
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        print(response.body);
+        print(secondEncode);
+        SnackBar(content: Text("Изображение профиля изменено."));
+      } else {
+        print(response.body);
+        print(secondEncode);
+        SnackBar(content: Text("Не удалось загрузить изображение профиля!"));
+      }
+    }
+  }
 }
