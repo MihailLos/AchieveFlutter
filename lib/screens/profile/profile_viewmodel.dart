60170b21e98ad6729a3a1d5bc3dbbfe926b320f0
ender <enderclub12@gmail.com>
2022-03-12 12:18:53 +0700

    Обертка получаемых данных из api во Future, добавление достижений в профиль

--- lib/screens/profile/profile_viewmodel.dart
+++ lib/screens/profile/profile_viewmodel.dart
@@ -1,23 +1,19 @@
 import 'dart:convert';
-import 'dart:ffi';
+import 'dart:developer';
 import 'dart:io';
 import 'dart:typed_data';
-
 
 import 'package:achieve_student_flutter/model/student_profile.dart';
 import 'package:achieve_student_flutter/network_handler.dart';
 import 'package:achieve_student_flutter/screens/login/login_screen.dart';
-import 'package:base_x/base_x.dart';
 
 import 'package:achieve_student_flutter/screens/report/report_screen.dart';
-import 'package:convert/convert.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:image/image.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:stacked/stacked.dart';
-import 'package:path/path.dart';
 
 class ProfileViewModel extends BaseViewModel {
   ProfileViewModel(BuildContext context);
@@ -27,20 +23,19 @@ class ProfileViewModel extends BaseViewModel {
   StudentProfileModel studentProfileModel = StudentProfileModel();
   int? studentPercent;
   bool circular = true;
-  bool imageCircular = true;
   FlutterSecureStorage storage = FlutterSecureStorage();
   double? studentPercentProgressBar;
+  bool? isCreated = true;
 
   Future onReady() async {
     fetchData();
     fetchStudentPercent();
-    circular = false;
-    notifyListeners();
   }
 
   void fetchData() async {
     var response = await networkHandler.get("/student/getStudent");
     studentProfileModel = StudentProfileModel.fromJson(response);
+    circular = false;
     notifyListeners();
   }
 
@@ -48,48 +43,109 @@ class ProfileViewModel extends BaseViewModel {
     var response = await networkHandler.get("/student/achievementsPercent");
     studentPercent = response;
     studentPercentProgressBar = studentPercent! / 100;
+    circular = false;
     notifyListeners();
   }
 
   exitButtonAction(context) {
     storage.delete(key: "token");
-    Navigator.pushAndRemoveUntil(context, new MaterialPageRoute(builder: (context) => LoginScreen()), (route) => false);
+    Navigator.pushAndRemoveUntil(
+        context,
+        new MaterialPageRoute(builder: (context) => LoginScreen()),
+        (route) => false);
   }
 
-  MemoryImage getImageFromBase64(String base64String) {
-    var firstDecode = base64Decode(base64String);
-    var tempString = latin1.decode(firstDecode).replaceAll(RegExp("[^A-Za-z0-9+/=]+"), "");
-    var secondDecode = base64Decode(tempString);
-    return MemoryImage(secondDecode);
+  Future<MemoryImage?> getImageFromBase64() async {
+    if (studentProfileModel.data == null) {
+      return null;
+    } else {
+      var firstDecode = base64Decode(studentProfileModel.data.toString());
+      var tempString = latin1.decode(firstDecode).replaceAll("\n", "");
+      var secondDecode = base64Decode(tempString);
+      return MemoryImage(secondDecode);
+    }
   }
 
-  getImageFromGallery() async {
-    String? accessToken = await storage.read(key: "token");
-    XFile? pickedFile = await ImagePicker()
-        .pickImage(source: ImageSource.gallery, maxWidth: 462, maxHeight: 462);
-    if (pickedFile != null) {
-      File imageFile = File(pickedFile.path);
-      var firstEncode = base64Encode(imageFile.readAsBytesSync());
-      Codec<String, String> stringToBase64Url = utf8.fuse(base64Url);
-      var secondEncode = stringToBase64Url.encode(firstEncode);
+  Future<String?> getName() async {
+    if (studentProfileModel.firstName == null && studentProfileModel.lastName == null) {
+      return null;
+    } else {
+      return "${studentProfileModel.firstName.toString()} ${studentProfileModel.lastName.toString()}";
+    }
+  }
+
+  Future<String?> getInstitute() async {
+    if (studentProfileModel.instituteFullName == null) {
+      return null;
+    } else {
+      return studentProfileModel.instituteFullName.toString();
+    }
+  }
+
+  Future<String?> getStream() async {
+    if (studentProfileModel.streamFullName == null) {
+      return null;
+    } else {
+      return studentProfileModel.streamFullName.toString();
+    }
+  }
+
+  Future<String?> getGroup() async {
+    if (studentProfileModel.groupName == null) {
+      return null;
+    } else {
+      return studentProfileModel.groupName.toString();
+    }
+  }
+
+  pickImage(ImageSource source, context) async {
+    var accessToken = await storage.read(key: "token");
+    try {
+      final image = await ImagePicker().pickImage(source: source);
+      File imageFile = File(image!.path);
+      var decodedImage = decodeImage(File(imageFile.path).readAsBytesSync());
+      var thumbnail = copyResize(decodedImage!, width: 120, height: 120);
+      File newFile = File(image.path)..writeAsBytesSync(encodePng(thumbnail));
+      var firstEncode = base64Encode(newFile.readAsBytesSync());
+      Codec<String, String> stringToBase64 = utf8.fuse(base64);
+      var secondEncode = stringToBase64.encode(firstEncode);
       Map<String, dynamic> bodyData = {
-        "data": firstEncode,
+        "data": secondEncode.toString(),
         "format": "png",
-        "listFileId": null
+        "listFileId": 3
       };
-      var response = await networkHandler.put("/student/changePhoto",
-          {"Authorization": "Bearer $accessToken"}, bodyData);
+
+      Map<String, String> headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $accessToken',
+      };
+      print(headers);
+
+      var response =
+          await networkHandler.put("/student/changePhoto", headers, bodyData);
+      print(response.statusCode);
 
       if (response.statusCode == 200 || response.statusCode == 201) {
-        print(response.body);
-        print(secondEncode);
-        SnackBar(content: Text("Изображение профиля изменено."));
+        refresh();
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("Фото профиля успешно изменено.")));
       } else {
-        print(response.body);
-        print(secondEncode);
-        SnackBar(content: Text("Не удалось загрузить изображение профиля!"));
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text("Ошибка при изменении изображения профиля!")));
       }
+    } on PlatformException catch (e) {
+      print("Не удалось загрузить изображение: $e");
     }
+  }
+
+  showCreatedAchieve() {
+    isCreated = true;
+    notifyListeners();
+  }
+
+  showReceivedAchieve() {
+    isCreated = false;
+    notifyListeners();
   }
 
   goToReportsScreen(context) {
@@ -100,5 +156,6 @@ class ProfileViewModel extends BaseViewModel {
   Future<void> refresh() async {
     fetchData();
     fetchStudentPercent();
+    showCreatedAchieve();
   }
 }
