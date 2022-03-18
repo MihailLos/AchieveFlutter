bfa64c3b23739e6dda6cd9abbbe48f557b28719a
ender <enderclub12@gmail.com>
2022-03-18 11:43:48 +0700

    Перемещение кода с виджетами из ViewModel

--- lib/screens/achievements/created_achieve_grid.dart
+++ lib/screens/achievements/created_achieve_grid.dart
@@ -15,7 +15,67 @@ class CreatedAchieveGrid extends StatelessWidget {
         viewModelBuilder: () => AchievementsViewModel(context),
         onModelReady: (viewModel) => viewModel.onReadyCreatedAchieveGrid(),
         builder: (context, model, child) {
-          return model.createdProfileAchievementsGrid(context);
+          return _createdProfileAchievementsGrid(context, model);
         });
+  }
+
+  _createdProfileAchievementsGrid(context, model) {
+    return model.createdProfileAchievements.isEmpty
+        ? Center(
+      child: CircularProgressIndicator(),
+    )
+        : GridView.builder(
+      physics: ScrollPhysics(),
+      shrinkWrap: true,
+      gridDelegate:
+      SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+      itemCount: model.createdProfileAchievements.length,
+      itemBuilder: (BuildContext context, int index) {
+        return FutureBuilder(
+          future: model.getImageForCreated(index),
+          builder: (context, snapshot) {
+            if (snapshot.hasData || snapshot.data != null) {
+              return InkWell(
+                onTap: () {},
+                child: Container(
+                  margin: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: snapshot.data as ImageProvider,
+                        fit: BoxFit.fill),
+                    border: Border.all(color: Colors.lightBlue, width: 4),
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                          color: Color(0xffbfbfbf),
+                          blurRadius: 6,
+                          spreadRadius: 2,
+                          offset: Offset(0, 4)),
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                          padding: EdgeInsets.all(4),
+                          child: Text(
+                            "${model.createdProfileAchievements[index].achieveName.toString()}",
+                            style: TextStyle(color: Colors.white),
+                          ))
+                    ],
+                  ),
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text("Произошла ошибка"),
+              );
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        );
+      },
+    );
   }
 }
