2c51b69aab673a1df0b60da2a41181e0b2fcb2f9
ender <enderclub12@gmail.com>
2022-03-24 10:39:15 +0700

    Изменение дизайна экрана создания сообщения об ошибке

--- lib/screens/report/newreport_screen.dart
+++ lib/screens/report/newreport_screen.dart
@@ -1,6 +1,7 @@
 import 'package:achieve_student_flutter/screens/report/report_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:google_language_fonts/google_language_fonts.dart';
 import 'package:stacked/stacked.dart';
 
 class NewReportScreenRoute extends MaterialPageRoute {
@@ -64,7 +65,7 @@ class NewReportScreen extends StatelessWidget {
       padding: const EdgeInsets.only(left: 20),
       child: Text(
         "Создание обращения",
-        style: GoogleFonts.montserrat(
+        style: CyrillicFonts.raleway(
             fontSize: 24,
             fontStyle: FontStyle.normal,
             fontWeight: FontWeight.bold,
@@ -80,10 +81,10 @@ class NewReportScreen extends StatelessWidget {
         Container(
           width: 334,
           child: TextField(
-            controller: model.themeReportController,
+              controller: model.themeReportController,
               decoration: InputDecoration(
-            hintText: "Тема обращения",
-          )),
+                hintText: "Тема обращения",
+              )),
         ),
       ],
     );
@@ -96,59 +97,62 @@ class NewReportScreen extends StatelessWidget {
         Container(
           width: 334,
           child: TextField(
-            controller: model.descriptionReportController,
-            maxLines: 7,
+              controller: model.descriptionReportController,
+              maxLines: 7,
               decoration: InputDecoration(
-            hintText: "Описание",
-            enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide(color: Color(0xFFC4C4C4), width: 1)),
-            focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide(color: Color(0xFFC4C4C4), width: 1)),
-          )),
+                hintText: "Описание",
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Color(0xFFC4C4C4), width: 1)),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Color(0xFFC4C4C4), width: 1)),
+              )),
         ),
       ],
     );
   }
 
   _sendReportButton(context, model) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-            width: 334,
-            height: 46,
-            decoration: BoxDecoration(
-              boxShadow: <BoxShadow>[
-                BoxShadow(
-                    color: Color(0xffbfbfbf),
-                    blurRadius: 6,
-                    spreadRadius: 2,
-                    offset: Offset(0, 4)),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 22),
+      child: Container(
+          width: double.maxFinite,
+          height: 46,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black45,
+                  offset: Offset(0, 6),
+                  spreadRadius: 1,
+                  blurRadius: 7
+              )
+            ],
+            borderRadius: BorderRadius.circular(10),
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color(0xFFFFBC89),
+                Color(0xFFFF9A67)
               ],
             ),
-            child: ElevatedButton(
-              onPressed: () async {
-                model.sendReportAction(context);
-              },
-              child: Text(
-                "Отправить",
-                style: TextStyle(
-                    fontFamily: "Montseratt",
-                    fontStyle: FontStyle.normal,
-                    fontSize: 17,
-                    fontWeight: FontWeight.w600),
-              ),
-              style: ElevatedButton.styleFrom(
-                shape: new RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                primary: Color(0xFFFF9966),
-              ),
-            )
-        ),
-      ],
+          ),
+          child: TextButton(
+            onPressed: () async {
+              model.sendReportAction(context);
+            },
+            child: Text(
+              "Отправить",
+              style: CyrillicFonts.raleway(
+                fontStyle: FontStyle.normal,
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,),
+            ),
+
+          )
+      ),
     );
   }
 }
