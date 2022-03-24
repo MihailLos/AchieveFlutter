7f3ce40b70dcb82c3fe61cc7bc51928434555cd0
ender <enderclub12@gmail.com>
2022-03-24 10:40:02 +0700

    Редизайн карточки сообщения об ошибке

--- lib/screens/report/report_card.dart
+++ lib/screens/report/report_card.dart
@@ -1,6 +1,7 @@
 import 'package:achieve_student_flutter/model/report.dart';
 import 'package:achieve_student_flutter/network_handler.dart';
 import 'package:flutter/material.dart';
+import 'package:google_language_fonts/google_language_fonts.dart';
 
 class ReportCard extends StatelessWidget {
   const ReportCard({Key? key, this.reportModel, this.networkHandler}) : super(key: key);
@@ -10,12 +11,48 @@ class ReportCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        title: Text(reportModel!.theme.toString() + " " + "(" + reportModel!.statusErrorName.toString() + ")"),
-        subtitle: Text(reportModel!.messageErrorDate.toString()),
-        isThreeLine: true,
-        trailing: Icon(reportModel!.statusErrorName.toString() == "Решено" ? Icons.check : reportModel!.statusErrorName.toString() == "Отклонено" ? Icons.highlight_off : reportModel!.statusErrorName.toString() == "Просмотрено" ? Icons.remove_red_eye : Icons.access_time),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                reportModel!.theme.toString(),
+                style: CyrillicFonts.raleway(
+                  fontStyle: FontStyle.normal,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w700),
+              ),
+              Text(
+                  reportModel!.statusErrorName.toString(),
+                  style: CyrillicFonts.openSans(
+                    fontStyle: FontStyle.normal,
+                    fontSize: 11,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black54)
+              ),
+              Text(
+                  reportModel!.messageErrorDate.toString(),
+                  style: CyrillicFonts.openSans(
+                      fontStyle: FontStyle.normal,
+                      fontSize: 11,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black54)
+              )
+            ],
+          ),
+          reportModel!.statusErrorName.toString() == "Решено" ?
+          Icon(Icons.check, color: Colors.green, size: 36,) :
+          reportModel!.statusErrorName.toString() == "Отклонено" ?
+          Icon(Icons.close, color: Colors.red, size: 36,) :
+          reportModel!.statusErrorName.toString() == "Просмотрено" ?
+          Icon(Icons.visibility, color: Colors.blueAccent, size: 36,) :
+          Icon(Icons.watch_later_outlined, color: Colors.black, size: 36,)
+        ],
       ),
     );
   }
