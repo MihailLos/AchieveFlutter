e95fb90943c5aeed6941ca14d33cdc8eb96b902c
ender <enderclub12@gmail.com>
2022-03-25 22:48:13 +0700

    Назначение анимации в качестве домашнего экрана

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'dart:io';
 
 import 'package:achieve_student_flutter/screens/home_view.dart';
 import 'package:achieve_student_flutter/screens/profile/profile_screen.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:achieve_student_flutter/screens/login/login_screen.dart';
 import 'package:flutter/services.dart';
+import 'package:google_language_fonts/google_language_fonts.dart';
+import 'package:lottie/lottie.dart';
 
 class MyHttpOverrides extends HttpOverrides{
   @override
@@ -25,6 +28,37 @@ void main() {
       primaryColor: Color(0xFF4065D8)
     ),
     debugShowCheckedModeBanner: false,
-    home: LoginScreen(),
+    home: SplashScreen(),
   ));
 }
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+        splash: Column(
+          children: [
+            Lottie.asset("assets/images/splash_logo.json"),
+            Text(
+              "Достижения",
+              style: CyrillicFonts.raleway(
+                  fontStyle: FontStyle.normal,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 1.68,
+                  color: Colors.white),
+            )
+          ],
+        ),
+        backgroundColor: Color(0xFF4065D8),
+        nextScreen: LoginScreen(),
+        splashIconSize: 500,
+        duration: 2000,
+        splashTransition: SplashTransition.fadeTransition,
+        animationDuration: Duration(seconds: 1),
+    );
+  }
+}
+
